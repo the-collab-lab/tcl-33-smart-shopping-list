2e682ce7afe81ea1ebb644d8464dad71bb6b8db7
Jennifer Toops <jennifertoops@gmail.com>
2021-10-03 18:44:43 -0400

    added components/AddItem with almost-functioning firebase code
    Co-authored-by: Arvin Fernandez arvinf07@gmail.com

---
+++ src/components/AddItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { db } from '../lib/firebase';
+// import firebase from 'firebase/app';
+import 'firebase/firestore';
+// import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+
+import { collection, doc, setDoc } from 'firebase/firestore';
+
+export default async function AddItem() {
+  const somethingRef = collection(db, 'something');
+
+  await setDoc(doc(somethingRef, 'SF'), {
+    name: 'San Francisco',
+    state: 'CA',
+    country: 'USA',
+    capital: false,
+    population: 860000,
+    regions: ['west_coast', 'norcal'],
+  });
+  // await setDoc(doc(citiesRef, "LA"), {
+  //     name: "Los Angeles", state: "CA", country: "USA",
+  //     capital: false, population: 3900000,
+  //     regions: ["west_coast", "socal"] });
+  // await setDoc(doc(citiesRef, "DC"), {
+  //     name: "Washington, D.C.", state: null, country: "USA",
+  //     capital: true, population: 680000,
+  //     regions: ["east_coast"] });
+  // await setDoc(doc(citiesRef, "TOK"), {
+  //     name: "Tokyo", state: null, country: "Japan",
+  //     capital: true, population: 9000000,
+  //     regions: ["kanto", "honshu"] });
+  // await setDoc(doc(citiesRef, "BJ"), {
+  //     name: "Beijing", state: null, country: "China",
+  //     capital: true, population: 21500000,
+  //     regions: ["jingjinji", "hebei"] });
+
+  return (
+    <div>
+      <h2>Testing 123</h2>
+    </div>
+  );
+}
