cde22291b5f384e15fbd2384691bf28e3b8cbf15
Manon Sainton <manonsainton.dev@gmail.com>
2021-10-05 16:14:41 -0400

    added view list and add item, is-ms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ViewList from './Pages/ViewList';
+import AddItem from './Pages/AddItem';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import { Route, Switch, BrowserRouter, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+          <div className="container mt-2" style={{ marginTop: 40 }}>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/AddItem">
+                <AddItem />
+              </Route>
+              <Route path="/ViewList">
+                <ViewList />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/AddItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddItem = () => (
+  <div>
+    <h1 className="title is-1">Add Item </h1>
+  </div>
+);
+
+export default AddItem;
