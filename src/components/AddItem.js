2545bd0657b346d65051d995b0297d458a2a3938
Arvin Fernandez <arvinf07@gmail.com>
2021-10-05 09:10:40 -0400

    renders loading and error messages from the firestore

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -14,29 +14,35 @@ export default function AddItem() {
   // regions: ["west_coast", "socal"] });
 
   // Write 1 document
-  // db.collection('lists').doc('4').set({
+  // db.collection('lists').add({
   //   name: 'Kiwi',
   //   price: 2.00,
   // });
 
   // Read 1 document
-  let docRef = db.collection('lists').doc('4');
-  docRef.get().then((doc) => console.log(doc.data()));
+  // let docRef = db.collection('lists').doc('4');
+  // docRef.get().then((doc) => console.log(doc.data()));
 
   const [value, loading, error] = useCollection(db.collection('lists'));
 
-  if (!loading) {
-    console.log(value);
-  }
+  const renderLists = () => {
+    if (!loading) {
+      return (
+        <ul>
+          {value.docs.map((doc) => (
+            <li key={doc.id}>{JSON.stringify(doc.data())}, </li>
+          ))}
+        </ul>
+      );
+    }
+  };
 
   return (
     <div>
       <h2>Testing 123</h2>
-      <ul>
-        {value.docs.map((doc) => (
-          <li key={doc.id}>{JSON.stringify(doc.data())}, </li>
-        ))}
-      </ul>
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {renderLists()}
 
       {/* <button onClick={}>Add Item</button> */}
     </div>
