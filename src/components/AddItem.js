7bad98a5022714cac273bd4a680d6d5545428f2f
Manon Sainton <manonsainton.dev@gmail.com>
2021-10-12 18:09:33 -0400

    handle change and submit

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react';
 import { db } from '../lib/firebase';
-import { useCollection } from 'react-firebase-hooks/firestore';
+import {
+  getToken,
+  words,
+  calculateEstimate,
+} from '@the-collab-lab/shopping-list-utils';
 
 export default function AddItem() {
   const [item, setItem] = useState('');
-  const [price, setPrice] = useState('');
+  const [urgency, setUrgency] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(item, urgency);
     e.target.reset();
     db.collection('lists').add({
       item,
-      price,
+      urgency,
+      // getToken()
     });
     console.log('submitted');
   };
 
-  const handleChange = (e) => {
-    console.log('changed');
-  };
-
   return (
     <div>
-      <form onSubmit={handleSubmit} onChange={handleChange}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="item">Grocery item:</label> <br />
         <input
           type="text"
@@ -31,33 +33,27 @@ export default function AddItem() {
           onChange={(e) => setItem(e.target.value)}
         />{' '}
         <br />
-        <label htmlFor="price">Price:</label> <br />
-        <input
-          type="text"
-          id="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />{' '}
-        <br />
+        <div onChange={(e) => setUrgency(parseInt(e.target.value))}>
+          <input type="radio" id="Soon" value="7" name="Urgency" />
+          <label htmlFor="Soon">Soon</label>
+          <input
+            htmlFor="Urgency"
+            id="Kind of Soon"
+            type="radio"
+            value="14"
+            name="Urgency"
+          />
+          <label htmlFor="Kind of Soon">Kind of Soon</label>
+          <input
+            htmlFor="Urgency"
+            id="Not Soon"
+            type="radio"
+            value="30"
+            name="Urgency"
+          />
+          <label htmlFor="Not Soon"> Not Soon</label>
+        </div>
         <input type="submit" value="submit" />
-        <input type="radio" id="Soon" value="7" name="Urgency" />
-        <label htmlFor="Soon">Soon</label>
-        <input
-          htmlFor="Urgency"
-          id="Kind of Soon"
-          type="radio"
-          value="14"
-          name="Urgency"
-        />
-        <label htmlFor="Kind of Soon">Kind of Soon</label>
-        <input
-          htmlFor="Urgency"
-          id="Not Soon"
-          type="radio"
-          value="30"
-          name="Urgency"
-        />
-        <label htmlFor="Not Soon"> Not Soon</label>
       </form>
     </div>
   );
