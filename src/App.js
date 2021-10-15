455503b1f8e02a0ae457caa8e9901e0648a48d16
Jennifer Toops <jennifertoops@gmail.com>
2021-10-15 12:25:48 -0400

    removed add list form, added logic to redirect user w/token to view list

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
-import NewList from './Pages/NewList';
+import React, { useState, useEffect } from 'react';
 import ViewList from './Pages/ViewList';
 import Navbar from './components/Navbar';
 import Home from './Pages/Home';
-import { Route, Switch, BrowserRouter, Link } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import './App.css';
 import AddItem from './components/AddItem';
-// import CreateList from './components/CreateList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="container mt-2" style={{ marginTop: 40 }}>
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/NewList">
-                <NewList />
-              </Route>
-              <Route path="/AddItem">
-                <AddItem />
-              </Route>
-              <Route path="/ViewList">
-                <ViewList />
-              </Route>
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const user = localStorage.getItem('Token');
+    user && setToken(user);
+    console.log(token);
+  }, [token]);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container mt-2" style={{ marginTop: 40 }}>
+          <Switch>
+            <Route exact path="/">
+              {token ? <Redirect to="/ViewList" /> : <Home />}
+            </Route>
+            <Route path="/AddItem">
+              <AddItem />
+            </Route>
+            <Route path="/ViewList">
+              <ViewList />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/CreateList.js
+++ src/components/CreateList.js
@@ -5,36 +5,19 @@ import { useHistory } from 'react-router-dom';
 function CreateList() {
   const history = useHistory();
 
-  function CreateToken() {
-    const Token = getToken(words);
-    localStorage.setItem(Token, Token);
+  function createToken() {
+    const token = getToken(words);
+    localStorage.setItem('Token', token);
   }
 
   function handleClick() {
-    history.push('/ViewList');
-    CreateToken();
-  }
-
-  //onSubmit will require conditional to search for input token and take user to populated list view
-  function onSubmit() {
+    createToken();
     history.push('/ViewList');
   }
 
   return (
     <div>
       <button onClick={handleClick}>Create Shopping List!</button>
-      <p>
-        {/*can make make "join an existing shopping list" clickable to open up the form*/}
-        You can also{' '}
-        <span>
-          <b>join an existing shopping list</b>
-        </span>
-      </p>
-
-      <form onSubmit={onSubmit}>
-        <input type="text" required minlength="10" /> <br />
-        <button type="submit">Get List</button>
-      </form>
     </div>
   );
 }
