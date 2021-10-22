44a8377dfdb1dccdd77429c7260d77c6bba06e2d
Arvin Fernandez <arvinf07@gmail.com>
2021-10-22 19:36:29 -0400

    created collectionExists method to check for valid collections

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home';
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import './App.css';
 import AddItem from './Pages/AddItem';
+import { db } from './lib/firebase';
 
 function App() {
   const [token, setToken] = useState(null);
@@ -15,11 +16,24 @@ function App() {
     user && setToken(user);
   }, [token]);
 
+  // db.collection(id).get().then(resp => !!resp.size)
+  const collectionExists = async (id) => {
+    let resp = await db.collection(id).get();
+    return !!resp.size;
+  };
+  console.log(
+    collectionExists('ncaa happy croak').then((value) => console.log(value)),
+  );
+
   const createToken = (e) => {
     e.preventDefault();
     const newToken = getToken(words);
     localStorage.setItem('Token', newToken);
     setToken(newToken);
+  };
+
+  const shareToken = (e, token) => {
+    e.preventDefault();
   };
 
   return (
