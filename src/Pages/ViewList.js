79d38ae44dcced4e51265dd88a3152ac52d6e0a0
Arvin Fernandez <arvinf07@gmail.com>
2021-10-29 22:12:49 -0400

    ViewList renders prompt to add item when collection is empty

--- src/Pages/ViewList.js
+++ src/Pages/ViewList.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { render } from 'react-dom';
 import { useCollection } from 'react-firebase-hooks/firestore';
+import EmptyListPrompt from '../components/EmptyListPrompt';
 import { db } from '../lib/firebase';
 
 const ViewList = ({ token }) => {
@@ -7,20 +9,31 @@ const ViewList = ({ token }) => {
 
   const [list, loading, error] = useCollection(db.collection(token));
 
+  const renderList = () => {
+    if (list.docs.length === 0) {
+      return <EmptyListPrompt />;
+    } else {
+      return (
+        <>
+          {error && <strong>Error: {JSON.stringify(error)}</strong>}
+          {list && (
+            <ul>
+              <b>Collection: </b>
+              {list.docs.map((doc) => (
+                <li key={doc.id}>{JSON.stringify(doc.data().item)} </li>
+              ))}
+            </ul>
+          )}
+        </>
+      );
+    }
+  };
+
   return (
     <div>
       <h2>Grocery List</h2>
 
-      {error && <strong>Error: {JSON.stringify(error)}</strong>}
-      {loading && <span>Collection: Loading...</span>}
-      {list && (
-        <ul>
-          <b>Collection: </b>
-          {list.docs.map((doc) => (
-            <li key={doc.id}>{JSON.stringify(doc.data().item)} </li>
-          ))}
-        </ul>
-      )}
+      {loading ? <span>Collection: Loading...</span> : renderList()}
     </div>
   );
 };
--- src/components/EmptyListPrompt.js
+++ src/components/EmptyListPrompt.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function EmptyListPrompt() {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Your list is currently empty...</h3>
+      <Link to="/AddItem">Add Item</Link>
+    </div>
+  );
 }
 
 export default EmptyListPrompt;
