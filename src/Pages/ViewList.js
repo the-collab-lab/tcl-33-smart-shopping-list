e3032e375c2f82b078ad4927c50dde1c3bf5134f
Jennifer Toops <jennifertoops@gmail.com>
2021-11-03 20:49:22 -0400

    created dummy array to test filter functionality

--- src/Pages/ViewList.js
+++ src/Pages/ViewList.js
@@ -3,6 +3,8 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../lib/firebase';
 import FilterList from '../components/FilterList';
 
+import { doc, onSnapshot } from 'firebase/firestore';
+
 const ViewList = ({ token }) => {
   // if (token === null) token = 'default';
 
@@ -13,7 +15,11 @@ const ViewList = ({ token }) => {
       <h2>Grocery List</h2>
 
       <FilterList />
-      {/*  */}
+      {/* need conditional that filters through document.data.item and matches them to const regex = /input.value/gmi
+      const filteredDocs = {list.docs.filter((doc.data().item) => ( doc.data().item.match(regex))
+            <li key={doc.id}>{JSON.stringify(doc.data().item)} </li>
+          ))}
+          */}
 
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,27 +1,51 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+let arrString = ['fruit', 'vegetables', 'water', 'legumes'];
 const FilterList = () => {
   const [value, setValue] = useState('');
 
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    console.log('Use Effect ');
+    if (value === '') {
+      setResults([]);
+      return;
+    }
+    const matches = arrString.filter(function (currentWord) {
+      console.log(`Current Word = ${currentWord}`);
+      return currentWord.includes(value);
+    });
+    console.log(matches);
+    setResults(matches);
+  }, [value]);
+
   const handleChange = (e) => {
-    console.log(e.target.value);
+    console.log('handle change fired');
     setValue(e.target.value);
   };
 
+  //   const handleReset = (e) => {
+  //       e.preventDefault();
+  //       console.log("reset");
+  //       setValue('');
+  //   }
+
   return (
     <div>
+      <p>Items = {results}</p>
       <form action="#">
         <label htmlFor="filter-list" style={{ display: 'hidden' }}>
           Filter List
         </label>
         <br />
         <input
-          type="text"
+          type="search"
           id="filter-list"
           value={value}
           onChange={handleChange}
           placeholder="Enter your item"
         />
+        {/* <button onClick={handleReset}>Reset</button> */}
       </form>
     </div>
   );
