bd65ce4a3326a0a5eef4888ab06aa65d69d086f2
Jennifer Toops <jennifertoops@gmail.com>
2021-11-07 17:01:33 -0500

    this was the version that worked until I refreshed it again

--- src/Pages/ViewList.js
+++ src/Pages/ViewList.js
@@ -1,5 +1,5 @@
 import { doc } from 'prettier';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../lib/firebase';
 
@@ -11,23 +11,11 @@ const ViewList = ({ token }) => {
   const [inputValue, setInputValue] = useState('');
   // const [matchValue, setMatchValue] = useState([]);
 
-  const handleChange = e => {
-    e.preventDefault()
+  const handleChange = (e) => {
+    e.preventDefault();
     console.log('handle change fired');
-    setInputValue(e.target.value)
+    setInputValue(e.target.value);
   };
-
-  // const findMatches = () => {
-  //   let matches = list.docs.map(doc => doc.data().item)
-  //   let matchesArray = JSON.stringify(matches).split(" ")
-  //   let filteredMatchesArray = matchesArray.filter(match => match.includes(inputValue))
-  //   setMatchValue(filteredMatchesArray)
-  //   console.log(`this is our matchesArray ${filteredMatchesArray} and this is our input ${inputValue}`)
-  // }
-
-
-
-  /* ******************************************* */
 
   return (
     <div>
@@ -52,31 +40,28 @@ const ViewList = ({ token }) => {
 
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
-      {list && !inputValue && inputValue == null ? (
+      {list && !inputValue ? (
         <ul>
-          {/* list.docs.filter(docs => docs.data().item.includes(inputValue).map(filteredItem => )) */}
           {list.docs.map((doc) => (
             <li key={doc.id} style={{ listStyleType: 'none' }}>
               {doc.data().item}
-              {/* {`this is our matchValue: ${matchValue}`} */}
-
-              {/* {JSON.stringify(filteredItems)} */}
               {console.log(JSON.stringify(doc.data().item))}{' '}
-              {/* {console.log("this is " + doc.data().item)} */}
             </li>
           ))}
         </ul>
-      ) : ( 
+      ) : (
         <ul>
           {list.docs.map((doc) => (
-          // <li>This is the input value {inputValue}</li>
-          <li key={doc.id} style={{ listStyleType: 'none' }}>
-            { doc.data().item.split(" ").filter(thing => thing.toLowerCase().includes(inputValue.toLowerCase()))}
+            <li key={doc.id} style={{ listStyleType: 'none' }}>
+              {doc
+                .data()
+                .item.split(' ')
+                .filter((thing) =>
+                  thing.toLowerCase().includes(inputValue.toLowerCase()),
+                )}
             </li>
-           ))} 
+          ))}
         </ul>
-
-
       )}
     </div>
   );
