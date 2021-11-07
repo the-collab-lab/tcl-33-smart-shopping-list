69050f3d65245d26bd51e68d33547dc02d13f738
Jennifer Toops <jennifertoops@gmail.com>
2021-11-06 23:59:23 -0400

    removed useEffect

--- src/Pages/ViewList.js
+++ src/Pages/ViewList.js
@@ -1,37 +1,20 @@
+import { doc } from 'prettier';
 import React, { useState, useEffect } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../lib/firebase';
-
-import { doc, onSnapshot } from 'firebase/firestore';
-
-// import { doc, onSnapshot } from 'firebase/firestore';
 
 const ViewList = ({ token }) => {
   // if (token === null) token = 'default';
 
   const [list, loading, error] = useCollection(db.collection(token));
 
-  /* ********************************   */
+  const [inputValue, setInputValue] = useState('');
 
-  let arrString = ['fruit', 'vegetables', 'water', 'legumes'];
-
-  const [value, setValue] = useState('');
-  const [results, setResults] = useState([]);
-
-  useEffect(() => {
-    console.log('Use Effect ');
-    if (value === '') {
-      setResults([]);
-      return;
-    }
-    const matches = list.docs.map((doc) => console.log(doc.data().item));
-    console.log(matches);
-    setResults(matches);
-  }, [value]);
+  // const filteredItems = list.docs.filter(doc => doc.data().item.includes(inputValue))
 
   const handleChange = (e) => {
     console.log('handle change fired');
-    setValue(e.target.value);
+    setInputValue(e.target.value);
   };
   /* ******************************************* */
 
@@ -41,7 +24,6 @@ const ViewList = ({ token }) => {
 
       {/* FILTER LIST RENDERING */}
       <div>
-        <p>Items = {results}</p>
         <form action="#">
           <label htmlFor="filter-list" style={{ display: 'hidden' }}>
             Filter List
@@ -50,7 +32,7 @@ const ViewList = ({ token }) => {
           <input
             type="search"
             id="filter-list"
-            value={value}
+            value={inputValue}
             onChange={handleChange}
             placeholder="Enter your item"
           />
@@ -59,17 +41,20 @@ const ViewList = ({ token }) => {
 
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
-      {list && !value ? (
+      {list && inputValue ? (
         <ul>
-          <b>Collection: </b>
+          {/* list.docs.filter(docs => docs.data().item.includes(inputValue).map(filteredItem => )) */}
           {list.docs.map((doc) => (
             <li key={doc.id} style={{ listStyleType: 'none' }}>
-              {JSON.stringify(doc.data().item)}{' '}
+              {JSON.stringify(doc.item)}
+              {console.log(inputValue)}
+              {/* {JSON.stringify(filteredItems)} */}
+              {console.log(JSON.stringify(doc.data().item))}{' '}
             </li>
           ))}
         </ul>
       ) : (
-        console.log('there is a value')
+        console.log('there is no inputValue')
       )}
     </div>
   );
